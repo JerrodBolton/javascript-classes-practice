6c281d117ecf849856252086045bce7cf6da3581
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-09-08 17:28:12 -0500

    Review 2

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ let ProductInfo =[
 ];
 
 // Create two instances of the class and call the method created to print the product's details.
-ProductInfo[0].print(); // this is going to print things out that I need. 
+// ProductInfo[0].print(); // this is going to print things out that I need. 
 
 
 
@@ -39,31 +39,67 @@ class PetInfoBlueprint{
      this.name = name;
      this.breed = breed; 
      this.age = age;
-     this.type = type; 
+     this.type = type;
  }
 
 // Create a method called formatProperties that prints all object properties using string templating. 
     print(){
         //  for the years part you may want to add a if statement
         console.log( `Hey my name is ${this.name}, and am a ${this.breed}; I'm ${this.age} years old`);
-        
+    };
+
+    // Create a method called changeAge that takes a number and updates the age property of an object.
+
+    ageChanger(newAge){
+            //  I want to change the info about my age.
+         newAge = prompt("what is your age");
+         this.age = newAge; // by putting this like this I written the age a different number .
+        console.log( `Hey my name is ${this.name}, and am a ${this.breed}; I'm ${this.age} years old`);
+         
+    };
+    // Create a method called dogYears that checks if the pet type of the object is dog, if so return the age in dog years (age times 7), otherwise return the message "[PET TYPE] is not a dog!". 
+    // maybe I should do a foreach in this one 
+    checkForDog(){
+        if(this.type === "DOG"){
+             this.age = this.age + 7 
+             console.log( `Hey my name is ${this.name}, and am a ${this.breed}; I'm ${this.age} years old`);
+        }
+       else{
+        console.log( `Hey my name is ${this.name}, and am a ${this.breed}; I'm ${this.age} years old`);
+        alert("not a dog"); 
+        };
     };
 };
+
+this.age = parseFloat(this.age);// I want to make this a number a number not a string 
 
 
 
 
 let PetInfo = [
- new PetInfoBlueprint( "Tim", "Western Harvest Mouse" , 5 ," Mouse")
+ new PetInfoBlueprint( "Tim","Western Harvest Mouse" , 5 ,"Mouse"),
+ new PetInfoBlueprint("Jame","Alaskan Malamute", 6 , "DOG"),
+ new PetInfoBlueprint("Tom","American Bulldogs", 9, "DOG" )
+
 ]
 
-
-// Create a method called changeAge that takes a number and updates the age property of an object. 
-// Create a method called dogYears that checks if the pet type of the object is dog, if so return the age in dog years (age times 7), otherwise return the message "[PET TYPE] is not a dog!". 
+// I can add a for each
 // Create an instance of the Pet Class called pet1. Print all property values of the object using the class method. 
 // Create another instance of the Pet Class with diffrent property values called pet2. 
-// Print all property values of the object using the class method. Change the age grade of pet1. 
-// Print the age of pet1 in dog years using the class method. Print the age of pet2 in dog years using the class method
+// PetInfo[0].print(); //this is going to print the information above
+// PetInfo[1].print();// this is a Dog 
+// PetInfo[2].print();// this is a Dog
+// PetInfo[1].ageChanger();// Print all property values of the object using the class method. Change the age grade of pet1. 
+
+
+
+// // Print the age of pet1 in dog years using the class method. Print the age of pet2 in dog years using the class method
+// PetInfo[0].checkForDog();
+// PetInfo[1].checkForDog();
+// PetInfo[2].checkForDog();
+
+
+
 
 
 
